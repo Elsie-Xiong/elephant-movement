7e585683d91d071703d98975bf9f9b10972f587c
Elsie-Xiong <m13907202077@163.com>
2026-09-23 15:24:32 +0800

    Fetch raw elephant tracking data

--- fetch.py
+++ fetch.py
@@ -18,9 +18,8 @@ from pathlib import Path
 
 import requests
 
-URL = ("https://data.weather.gov.hk/weatherAPI/opendata/opendata.php"
-       "?dataType=CLMTEMP&rformat=csv&station=HKO&year=2026")      # CHANGE ME
-FILE = "hko-daily-mean-temperature-2026.csv"                          # CHANGE ME: say what it is,
+URL = "https://datarepository.movebank.org/server/api/core/bitstreams/10240da1-0a99-42bf-a34f-eff15883bbfb/content"   # CHANGE ME
+FILE = "ThermochronTracking Elephants Kruger 2007.csv"                     # CHANGE ME: say what it is,
                                                                       # keep the publisher's extension
 HERE = Path(__file__).parent
 DATA = HERE / "data"
